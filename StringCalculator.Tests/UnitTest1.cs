0a287bd8c16a9eecfec6e8399b800d00d8603ae9
MarkBoyle1 <mark.boyle@myob.com>
2021-08-31 13:55:16 +1000

    Step 7 Complete: Throws exception for negative numbers

--- StringCalculator.Tests/UnitTest1.cs
+++ StringCalculator.Tests/UnitTest1.cs
@@ -7,47 +7,54 @@ namespace StringCalculator.Tests
 {
     public class UnitTest1
     {
+        private Calculator _calculator = new Calculator();
         [Fact]
         public void Test1()
         {
-            Assert.Equal(0, Calculator.Add(""));
+            Assert.Equal(0, _calculator.Add(""));
         }
         
         [Fact]
         public void Test2()
         {
-            Assert.Equal(1, Calculator.Add("1"));
-            Assert.Equal(3, Calculator.Add("3"));
+            Assert.Equal(1, _calculator.Add("1"));
+            Assert.Equal(3, _calculator.Add("3"));
 
         }
         
         [Fact]
         public void Test3()
         {
-            Assert.Equal(3, Calculator.Add("1,2"));
-            Assert.Equal(8, Calculator.Add("3,5"));
+            Assert.Equal(3, _calculator.Add("1,2"));
+            Assert.Equal(8, _calculator.Add("3,5"));
 
         }
 
         [Fact]
         public void Test4()
         {
-            Assert.Equal(6, Calculator.Add("1,2,3"));
-            Assert.Equal(20, Calculator.Add("3,5,3,9"));
+            Assert.Equal(6, _calculator.Add("1,2,3"));
+            Assert.Equal(20, _calculator.Add("3,5,3,9"));
 
         }
 
         [Fact]
         public void Test5()
         {
-            Assert.Equal(6, Calculator.Add("1,2\n3"));
-            Assert.Equal(20, Calculator.Add("3\n5\n3,9"));
+            Assert.Equal(6, _calculator.Add("1,2\n3"));
+            Assert.Equal(20, _calculator.Add("3\n5\n3,9"));
 
         }
         [Fact]
         public void Test6()
         {
-            Assert.Equal(3, Calculator.Add("//;\n1;2"));
+            Assert.Equal(3, _calculator.Add("//;\n1;2"));
+        }
+        [Fact]
+        public void Test7()
+        {
+            var ex = Assert.Throws<Exception>(() => _calculator.Add("-1,2,-3"));
+            Assert.Equal("Negatives not allowed: -1, -3", ex.Message);
         }
     }
 }
--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace StringCalculator
 {
@@ -6,24 +9,40 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Add("//;\n1;2"));
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add("//;\n1;2,3,6,7"));
         }
-        public static int Add(string input)
+        public int Add(string input)
         {
             if (String.IsNullOrEmpty(input)) return 0;
-            char[] charArray = input.ToCharArray();
             if (input.Contains("//")) 
-                input = input.Replace(charArray[2], ',');
+                input = input.Replace(input[2], ',');
             string[] delimiters = new string[] {",", "\n"};
             string[] numbers = input.Split(delimiters, input.Length, StringSplitOptions.None);
             int answer = 0;
+            List<string> negativeNumbers = new List<string>();
             foreach (string number in numbers)
             {
                 int intNumber;
-                if(int.TryParse(number, out intNumber))
-                    answer += int.Parse(number);
+                if (int.TryParse(number, out intNumber))
+                    if (int.Parse(number) < 0)
+                    {
+                        negativeNumbers.Add(number);
+                    }
+                    else
+                    {
+                        answer += int.Parse(number);
+                    }
             }
+
+            if (negativeNumbers.Count > 0) ThrowNegativeNumberException(negativeNumbers);
             return answer;
+        }
+
+        private void ThrowNegativeNumberException(List<string> negativeNumbers)
+        {
+            string errorMessage = "Negatives not allowed: " + negativeNumbers.Aggregate((a, b) => a + ", " + b);
+            throw new Exception(errorMessage);
         }
     }
 }
